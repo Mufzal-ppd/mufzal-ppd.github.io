ffa1dffda03e7743bde25e112fdd9ee7ea49e05b
Mufzal-ppd <mufzal.ppd@gmail.com>
2025-10-01 07:36:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/mufzal-portfolio-bolt/", // repo name here
+  base: "/", // 👈 use just "/" because it's now the root domain
   plugins: [react()],
 })
